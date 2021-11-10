11eed758a9bf076ce299ad6e5ef7b6bb22f40aac
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-10 23:42:04 +0100

    Adjusted constructor.

--- src/com/szymonjasinski/Car.java
+++ src/com/szymonjasinski/Car.java
@@ -1,15 +1,19 @@
 package com.szymonjasinski;
 
 public class Car {
-    private final String producer;
-    private final String model;
+    private String producer;
+    private String model;
     private Integer age;
     private Double mileage;
 
     public Car(String producer, String model, Integer age, Double mileage) {
-        this.producer = producer;
-        this.model = model;
-        this.age = age;
-        this.mileage = mileage;
+        if (age > 0 && mileage > 0) {
+            this.producer = producer;
+            this.model = model;
+            this.age = age;
+            this.mileage = mileage;
+        } else {
+            System.out.println("ERROR/. This car have a negative value of age and/or mileage.");
+        }
     }
 }
