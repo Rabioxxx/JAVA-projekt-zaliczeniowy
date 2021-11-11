27a9c7c036b731cfc5f5d113f0510d2c481d7e23
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-11 23:46:04 +0100

    Added getter setter

--- src/com/szymonjasinski/Player.java
+++ src/com/szymonjasinski/Player.java
@@ -6,4 +6,28 @@ public class Player {
     private static Double cash;
     private static ArrayList<Car> cars;
     private static ArrayList<Client> clients;
+
+    public static Double getCash() {
+        return cash;
+    }
+
+    public static void setCash(Double cash) {
+        Player.cash = cash;
+    }
+
+    public static ArrayList<Car> getCars() {
+        return cars;
+    }
+
+    public static void setCars(ArrayList<Car> cars) {
+        Player.cars = cars;
+    }
+
+    public static ArrayList<Client> getClients() {
+        return clients;
+    }
+
+    public static void setClients(ArrayList<Client> clients) {
+        Player.clients = clients;
+    }
 }
