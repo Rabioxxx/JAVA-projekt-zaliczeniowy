851243a707eb528441a59f65a27878f5b846e4ee
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-11 22:07:23 +0100

    market first added

---
+++ src/com/szymonjasinski/Market.java
@@ -0,0 +1,15 @@
+package com.szymonjasinski;
+
+import java.util.ArrayList;
+
+public class Market {
+    private ArrayList<Car> carsBuyable;
+
+    public ArrayList<Car> getCarsBuyable() {
+        return carsBuyable;
+    }
+
+    public void setCarsBuyable(ArrayList<Car> carsBuyable) {
+        this.carsBuyable = carsBuyable;
+    }
+}
