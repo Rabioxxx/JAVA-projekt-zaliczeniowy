8e8b35eea9e059f5d878cbcc9c1aef6fda116903
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-11 15:57:54 +0100

    Implemented first vars.

--- src/com/szymonjasinski/Player.java
+++ src/com/szymonjasinski/Player.java
@@ -1,4 +1,9 @@
 package com.szymonjasinski;
 
+import java.util.ArrayList;
+
 public class Player {
+    private static Double cash;
+    private static ArrayList<Car> carsOwned;
+    static ArrayList<Car> carsBuyable;
 }
