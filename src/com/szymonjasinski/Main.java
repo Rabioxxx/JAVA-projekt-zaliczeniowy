26a7a3ff5189daf47f534f6dc4ef671f1a1a74b1
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-11 13:27:28 +0100

    printAndWaitForInput + adjustments.

--- src/com/szymonjasinski/Main.java
+++ src/com/szymonjasinski/Main.java
@@ -4,40 +4,45 @@ import java.util.Scanner;
 
 public class Main {
 
+    static Scanner scanner = new Scanner(System.in);
+    static char input;
+
     public static void main(String[] args) {
 
+        printAndWaitForInput(
+                """
+                        Wanna play some game?
+                        a - new game
+                        b - exit program"""
+        );
 
+        printAndWaitForInput(
+                """
+                                                    
+                        What do you want to do now?
+                                                    
+                        a - show cars available at https://www.cars.com/.
+                        b - show cars you own.
+                        c - show clients.
+                        d - check marketing options.
+                        e - check you bank account.""");
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        char input;
+    static void printAndWaitForInput(String message) {
+
+        scanner = new Scanner(System.in);
 
         do {
-            System.out.println(
-                    """
-                                                        
-                            What do you want to do now?
-                                                        
-                            a - show cars available at https://www.cars.com/.
-                            b - show cars you own.
-                            c - show clients.
-                            d - check marketing options.
-                            e - check you bank account.""");
+            System.out.println("\n" + message);
 
+            // Wait for user input and assign value to variable input.
             input = scanner.next().charAt(0);
 
             // TODO Could change a, b, c ... to interval from 97 (lowercase a) to 122 (lowercase z)
             switch (input) {
-                case 'a' -> System.out.println(input + " clicked.");
-                case 'b' -> System.out.println(input + " clicked.");
-                case 'c' -> System.out.println(input + " clicked.");
-                case 'd' -> System.out.println(input + " clicked.");
-                case 'e' -> System.out.println(input + " clicked.");
+                case 'a', 'b', 'c', 'd', 'e' -> System.out.println(input + " clicked.");
                 default -> System.out.println(">:(");
             }
-        } while (input != 'a'
-                && input != 'b'
-                && input != 'c'
-                && input != 'd'
-                && input != 'e');
+        } while (input < 97 || input > 122);
     }
 }
