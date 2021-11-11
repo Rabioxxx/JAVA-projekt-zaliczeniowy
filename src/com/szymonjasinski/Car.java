2d84926a9c5785b8293edadd7e5785aea1c0e626
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-11 15:57:20 +0100

    Constructor + vars value, color, cargo

--- src/com/szymonjasinski/Car.java
+++ src/com/szymonjasinski/Car.java
@@ -1,17 +1,25 @@
 package com.szymonjasinski;
 
+import java.awt.*;
+
 public class Car {
     private String producer;
     private String model;
     private Integer age;
     private Double mileage;
+    private Double value;
+    private Color color;
+    private Boolean cargo; // Ładownia - yes or no.
 
-    public Car(String producer, String model, Integer age, Double mileage) {
-        if (age > 0 && mileage > 0) {
+    public Car(String producer, String model, Integer age, Double mileage, Double value, Color color, Boolean cargo) {
+        if (age >= 0 && mileage >= 0) {
             this.producer = producer;
             this.model = model;
             this.age = age;
             this.mileage = mileage;
+            this.value = value;
+            this.color = color;
+            this.cargo = cargo;
         } else {
             System.out.println("ERROR/. This car have a negative value of age and/or mileage.");
         }
