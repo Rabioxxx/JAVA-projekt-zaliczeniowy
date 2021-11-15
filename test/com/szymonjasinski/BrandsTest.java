cf036a378a1f5c78cac2a96e767ee76319e7607e
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-15 18:23:36 +0100

    just testing

--- test/com/szymonjasinski/BrandsTest.java
+++ test/com/szymonjasinski/BrandsTest.java
@@ -3,17 +3,17 @@ package com.szymonjasinski;
 import org.junit.jupiter.api.Test;
 
 import java.security.SecureRandom;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BrandsTest {
 
     @Test
-    void randomBrand() {
-    }
-
-    @Test
-    void getName() {
-
+    void Test() {
+        Brands brand = Brands.randomBrand();
+        System.out.println(brand.getName());
+        System.out.println(brand.getModel());
+        System.out.println(brand.getModels());
     }
 }
