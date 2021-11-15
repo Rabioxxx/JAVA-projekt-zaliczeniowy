7e244438bb326e51eaf7fa5c6a924d813d4f3b16
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-15 02:08:23 +0100

    Now I have enum Brands with specific models assigned to specific brand/producer.

--- src/com/szymonjasinski/Brands.java
+++ src/com/szymonjasinski/Brands.java
@@ -5,14 +5,47 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public enum Brands { // Here I can add more car brands.
-    ALFAROMEO("Alfa Romeo"), AUDI("Audi"), BMW("BMW"), CHEVROLET("Chevrolet"), CITROEN("Citroen"), DACIA("Dacia"), DAEWOO("Daewoo"), DODGE("Dodge"), FIAT("Fiat"), FORD("Ford"), HONDA("Honda"), HYUNDAI("Hyundai"), JAGUAR("Jaguar"), JEEP("Jeep"), KIA("KIA"), LEXUS("Lexus"), MAZDA("Mazda"), MERCEDES("Mercedes-Benz"), MITSUBISHI("Mitsubishi"), NISSAN("Nissan"), OPEL("Opel"), PEUGEOT("Peugeot"), RENAULT("Renault"), SAAB("Saab"), SEAT("Seat"), SKODA("Skoda"), SUBARU("Subaru"), SUZUKI("Suzuki"), TESLA("Tesla"), TOYOTA("Toyota"), VOLKSWAGEN("Volkswagen"), VOLVO("Volvo");
+public enum Brands { // Here I can add more car brands + their models!
+    ALFAROMEO("Alfa Romeo", new String[]{"500L", "500L Living", "500X Pulse", "Sedici", "Seiciento", "Multipla", "Siena", "Palio", "Marea", "Bravo", "Punto", "Cinquecento", "Tipo", "Tempra", "Croma", "Penny", "Marengo"}),
+    AUDI("Audi", new String[]{"500L", "500L Living", "500X Pulse"}),
+    BMW("BMW", new String[]{"500L", "500L Living", "500X Pulse"}),
+    CHEVROLET("Chevrolet", new String[]{"500L", "500L Living", "500X Pulse"}),
+    CITROEN("Citroen", new String[]{"500L", "500L Living", "500X Pulse"}),
+    DACIA("Dacia", new String[]{"500L", "500L Living", "500X Pulse"}),
+    DAEWOO("Daewoo", new String[]{"500L", "500L Living", "500X Pulse"}),
+    DODGE("Dodge", new String[]{"500L", "500L Living", "500X Pulse"}),
+    FIAT("Fiat", new String[]{"500L", "500L Living", "500X Pulse"}),
+    FORD("Ford", new String[]{"500L", "500L Living", "500X Pulse"}),
+    HONDA("Honda", new String[]{"500L", "500L Living", "500X Pulse"}),
+    HYUNDAI("Hyundai", new String[]{"500L", "500L Living", "500X Pulse"}),
+    JAGUAR("Jaguar", new String[]{"500L", "500L Living", "500X Pulse"}),
+    JEEP("Jeep", new String[]{"500L", "500L Living", "500X Pulse"}),
+    KIA("KIA", new String[]{"500L", "500L Living", "500X Pulse"}),
+    LEXUS("Lexus", new String[]{"500L", "500L Living", "500X Pulse"}),
+    MAZDA("Mazda", new String[]{"500L", "500L Living", "500X Pulse"}),
+    MERCEDES("Mercedes-Benz", new String[]{"500L", "500L Living", "500X Pulse"}),
+    MITSUBISHI("Mitsubishi", new String[]{"500L", "500L Living", "500X Pulse"}),
+    NISSAN("Nissan", new String[]{"500L", "500L Living", "500X Pulse"}),
+    OPEL("Opel", new String[]{"500L", "500L Living", "500X Pulse"}),
+    PEUGEOT("Peugeot", new String[]{"500L", "500L Living", "500X Pulse"}),
+    RENAULT("Renault", new String[]{"500L", "500L Living", "500X Pulse"}),
+    SAAB("Saab", new String[]{"500L", "500L Living", "500X Pulse"}),
+    SEAT("Seat", new String[]{"500L", "500L Living", "500X Pulse"}),
+    SKODA("Skoda", new String[]{"500L", "500L Living", "500X Pulse"}),
+    SUBARU("Subaru", new String[]{"500L", "500L Living", "500X Pulse"}),
+    SUZUKI("Suzuki", new String[]{"500L", "500L Living", "500X Pulse"}),
+    TESLA("Tesla", new String[]{"500L", "500L Living", "500X Pulse"}),
+    TOYOTA("Toyota", new String[]{"500L", "500L Living", "500X Pulse"}),
+    VOLKSWAGEN("Volkswagen", new String[]{"500L", "500L Living", "500X Pulse"}),
+    VOLVO("Volvo", new String[]{"500L", "500L Living", "500X Pulse"});
 
-    // I need a String value.
-    String name;
+    // I need a String value, so these values can have names.
+    private final String name;
+    private final String[] models;
 
-    Brands(String name) {
+    Brands(String name, String[] models) {
         this.name = name;
+        this.models = models;
     }
 
     // Creating new list of Brands with all the values from enum Brands.
