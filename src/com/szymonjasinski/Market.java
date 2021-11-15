6cb54d9fa19b53ca09c71d2b720b0c95ecd34118
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-15 01:46:20 +0100

    Implementing carsGenerator once more.

--- src/com/szymonjasinski/Market.java
+++ src/com/szymonjasinski/Market.java
@@ -1,17 +1,7 @@
 package com.szymonjasinski;
 
-import java.util.ArrayList;
-import java.util.Random;
-
-enum Brands{
-    ALFAROMEO("Alfa Romeo"), AUDI("Audi"), BMW("BMW"), CHEVROLET("Chevrolet"), CITROEN("Citroen"), DACIA("Dacia"), DAEWOO("Daewoo"), DODGE("Dodge"), FIAT("Fiat"), FORD("Ford"), HONDA("Honda"), HYUNDAI("Hyundai"), JAGUAR("Jaguar"), JEEP("Jeep"), KIA("KIA"), LEXUS("Lexus"), MAZDA("Mazda"), MERCEDES("Mercedes-Benz"), MITSUBISHI("Mitsubishi"), NISSAN("Nissan"), OPEL("Opel"), PEUGEOT("Peugeot"), RENAULT("Renault"), SAAB("Saab"), SEAT("Seat"), SKODA("Skoda"), SUBARU("Subaru"), SUZUKI("Suzuki"), TESLA("Tesla"), TOYOTA("Toyota"), VOLKSWAGEN("Volkswagen"), VOLVO("Volvo");
-
-    String name;
-
-    Brands(String name){
-        this.name = name;
-    }
-}
+import java.util.*;
+import java.lang.*;
 
 public class Market {
     private ArrayList<Car> cars;
@@ -26,11 +16,29 @@ public class Market {
 
     public void carsGenerator(Integer number) {
         // TODO #003
+        /*
         String[] producers = {"Fiat", "Opel", "Ford", "Mercedes-Benz", "Renault", "Tesla", "Toyota", "Hyundai", "BMW", "SAAB", "KIA", "Daewoo", "Honda", "Nissan", "Peugeot", "Volvo", "Suzuki", "Mazda", "Subaru", "Jeep", "Audi", "Jaguar", "Dodge", "Alfa Romeo", "Lexus", "Chevrolet", "Volkswagen", "Mitsubishi", "Citroen", "Skoda", "SEAT", "Dacia"};
         String[] models = {"500", "Insignia", "Fiesta", "CLA45", "Clio", "Model X"};
-        String[] fiatModels = {"500L", "500L Living", "500X", "Pulse", "Tipo", "Cronos", "Argo", "Uno", "Panda", "500", "New 500", "Mobi", /* up to this moment there are current models*/ "Sedici", "Seiciento", "Multipla", "Siena", "Palio", "Marea", "Bravo", "Punto", "Cinquecento", "Tipo", "Tempra", "Croma", "Penny", "Marengo"};
+        String[] fiatModels = {"500L", "500L Living", "500X", "Pulse", "Tipo", "Cronos", "Argo", "Uno", "Panda", "500", "New 500", "Mobi", *//* up to this moment there are current models*//* "Sedici", "Seiciento", "Multipla", "Siena", "Palio", "Marea", "Bravo", "Punto", "Cinquecento", "Tipo", "Tempra", "Croma", "Penny", "Marengo"};
+        */
 
-        if (number <= models.length) {
+
+        Random rng = new Random();
+
+        int ageRandom = rng.nextInt(30);
+        String brandRandom = Brands.randomBrand().getName(); // For random brand name. These methods will assign String.
+
+
+        ArrayList<Car> cars = new ArrayList<>();
+
+        Car carRandom = new Car(brandRandom, /*modelRandom*/ "", ageRandom, /*mileageRandom*/ 0.0, /*valueRandom*/ 0.0, /*colorRandom*/ Color.BLACK);
+        cars.add(carRandom);
+
+        setCars(cars);
+
+
+
+        /*if (number <= models.length) {
 
             Random rng = new Random();
 
@@ -51,6 +59,6 @@ public class Market {
 
         } else {
             throw new ArrayIndexOutOfBoundsException("Variable number cannot be bigger than producers[]/models[] index.");
-        }
+        }*/
     }
 }
