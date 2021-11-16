f0bcf1054cd6df107fb2485910cc4ad2f7d7c0d3
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-16 16:06:31 +0100

    new value for constants - "name".

--- src/com/szymonjasinski/Segment.java
+++ src/com/szymonjasinski/Segment.java
@@ -1,5 +1,30 @@
 package com.szymonjasinski;
 
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public enum Segment {
-    BUDGET, STANDARD, PREMIUM
+    BUDGET("Budget"),
+    STANDARD("Standard"),
+    PREMIUM("Premium");
+
+    private final String segment;
+
+    Segment(String segment) {
+        this.segment = segment;
+    }
+
+    // Creating new list of Segment with all the values from enum Segment.
+    private static final List<Segment> SEGMENT_LIST = Collections.unmodifiableList(Arrays.asList(values()));
+
+    //Now we check size of the list above.
+    private static final int SIZE = SEGMENT_LIST.size();
+
+    private static final SecureRandom RNG = new SecureRandom();
+
+    public String getName() {
+        return this.segment;
+    }
 }
