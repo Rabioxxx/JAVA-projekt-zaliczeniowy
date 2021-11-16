a3bbbccbc8f75e173840b6da1b7212ad0b0e59a6
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-16 16:35:47 +0100

    just tests

--- test/com/szymonjasinski/BrandTest.java
+++ test/com/szymonjasinski/BrandTest.java
@@ -2,10 +2,17 @@ package com.szymonjasinski;
 
 import org.junit.jupiter.api.Test;
 
+import java.text.DecimalFormat;
+
 class BrandTest {
 
     @Test
     void Test() {
-        Brand brand = Brand.randomBrand();
+        double mileageRandom = 97550.885200459;
+
+        System.out.printf("bla bla bla %.2f %n", mileageRandom);
+
+        DecimalFormat df = new DecimalFormat("#.0");
+        System.out.println(df.format(mileageRandom));
     }
 }
