5fce5196e88eb4f9c4f5cbe8021d53b0e8ce23b9
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-16 17:40:43 +0100

    test

--- test/com/szymonjasinski/BrandTest.java
+++ test/com/szymonjasinski/BrandTest.java
@@ -16,7 +16,17 @@ class BrandTest {
         System.out.println(df.format(mileageRandom));
 
         Brand bmw = Brand.BMW;
+        Brand audi = Brand.AUDI;
+        Brand opel = Brand.OPEL;
+        Brand volkswagen = Brand.VOLKSWAGEN;
+        Brand ford = Brand.FORD;
 
         System.out.println(bmw.getBrandCommonness());
+        System.out.println(audi.getBrandCommonness());
+        System.out.println(opel.getBrandCommonness());
+        System.out.println(volkswagen.getBrandCommonness());
+        System.out.println(ford.getBrandCommonness());
+
+        System.out.println(Brand.SUM_ALL_BRANDS_COMMONNESS);
     }
 }
