8e7ab99e9ad2447c0ff08094d29fb347303b7552
Szymon Jasiński <szg.jasinski@gmail.com>
2021-11-16 16:59:27 +0100

    Added method to randomize color from enum.

--- src/com/szymonjasinski/Color.java
+++ src/com/szymonjasinski/Color.java
@@ -1,5 +1,10 @@
 package com.szymonjasinski;
 
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public enum Color {
     BLACK("Black"),
     WHITE("White"),
@@ -13,7 +18,20 @@ public enum Color {
 
     private final String name;
 
+    // Creating new list of Brand with all the values from enum Brand.
+    private static final List<Color> COLOR_LIST = Collections.unmodifiableList(Arrays.asList(values()));
+
+    //Now we check size of the list above.
+    private static final int SIZE = COLOR_LIST.size();
+
+    private static final SecureRandom RNG = new SecureRandom();
+
     Color(String name) {
         this.name = name;
+    }
+
+    public static Color getRandomColor(){
+        int i = RNG.nextInt(SIZE);
+        return COLOR_LIST.get(i);
     }
 }
